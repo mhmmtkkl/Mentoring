8aac42abc377e9417e0f870726142379966256f0
Yusef <yusufesenboga42@gmail.com>
2020-03-31 20:36:53 -0400

    Add files via upload
    March 31st

---
+++ src/Yusuf/Task2.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Task2 {
+    public static void main(String[] args) {
+        //Task 2
+        //What time of the day is it?
+        //8 to 12 Good Morning
+        //12 to 18 Good Afternoon
+        //18 to 22 Good Evening
+        //After 22 Good Night
+        Scanner input = new Scanner(System.in);
+        System.out.print("What time is it? ");
+        int time = input.nextInt();
+
+        if(time >= 8 && time < 12){
+            System.out.println("Good Morning");
+        }
+        else if(time >= 12 && time <= 18){
+            System.out.println("Good Afternoon");
+        }
+        else if(time >= 18 && time < 22){
+            System.out.println("Good Evening");
+        }
+        else if(time >= 22){
+            System.out.println("Good Night");
+        }
+
+    }
+}
