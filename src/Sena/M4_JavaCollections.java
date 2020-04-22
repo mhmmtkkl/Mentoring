a971faac882c92cffcd88c150990af3814ad1c10
rsena <rsenaulgur@gmail.com>
2020-04-22 10:14:56 -0400

    mentorings added w/ presentation links

---
+++ src/Sena/M4_JavaCollections.java
@@ -0,0 +1,13 @@
+package Sena;
+
+public class M4_JavaCollections {
+
+    /**
+     * Presentation link:
+     * https://docs.google.com/presentation/d/1uOY8S3GTmqY1c6RFMs5_Gg83HU02NTGVpYzFvjWZ7WA/edit?usp=sharing
+     */
+
+    // Summary:
+
+    // Tasks:
+}
