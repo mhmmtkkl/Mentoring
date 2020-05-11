08c611d8fb770a07e76f7e1812565bd0cf50b076
yusufesenboga <yusufesenboga42@gmail.com>
2020-05-11 05:08:51 -0400

    Constructors

---
+++ src/Yusuf/Constructors/Task1.java
@@ -0,0 +1,10 @@
+package Yusuf.Constructors;
+public class Task1 {
+//    Write a class called Car and write the following field:
+//    String brand, int year, enum condition, double price
+//    fill your enum Condition with REALLYOLD, USED, GOOD, LIKENEW
+    
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Yusuf/Constructors/Task2.java
@@ -0,0 +1,7 @@
+package Yusuf.Constructors;
+
+public class Task2 {
+//    Create a constructor which will fill all the attributes that's listed in the Car class
+//    Create another constructor which will only fill brand and year.
+//    Also create a toString method to print it out at the end
+}
