b2e24d3e61b86d8c40d22c1a0a9c0f56c93a56d0
rsena <rsenaulgur@gmail.com>
2020-05-30 15:55:01 -0400

    new mentoring package added

---
+++ src/Sena/M8_Inheritance/Summary.java
@@ -0,0 +1,56 @@
+package Sena.M8_Inheritance;
+
+public class Summary {
+
+    /**
+     * Presentation link:
+     * https://docs.google.com/presentation/d/1Of3EntbM-1Hkqp8co5pcO1cQV8WyNW1HBvHRnAoyC2w/edit?usp=sharing
+     */
+
+    // Inheritance: parent class --> child class
+    // if parent is a class: use "extends" / parent classes can be used once per class
+    // if parent is an interface: use "implements" / interfaces can be used as much as needed per class
+
+    // Notes:
+    // private fields and methods are not shared
+    // if parent class has a constructor, constructor matching super has to be created
+    // if parent class has a method, method has to be overridden
+
+}
+
+class superclass {
+
+}
+
+class subclass1 {
+
+}
+
+//
+
+abstract class abstractSuperclass {
+
+}
+
+class subclass2 {
+
+}
+
+//
+
+interface superInterface1 {
+
+}
+
+interface superInterface2 {
+
+}
+
+class subclass3 {
+
+}
+
+
+
+
+
