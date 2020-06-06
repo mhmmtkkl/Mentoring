922f3b9e8e3a45718ac37cebf92a18b3e1d4fb3d
rsena <rsenaulgur@gmail.com>
2020-06-06 13:10:18 -0400

    modified after comments

--- src/Sena/M9_Interface/Summary.java
+++ src/Sena/M9_Interface/Summary.java
@@ -10,7 +10,7 @@ public class Summary {
     // Interface
     // In the Java programming language, an interface is a reference type, similar to a class,
     // that can contain only constants, method signatures, default methods, static methods, and nested types.
-    // Interfaces are 100% abstract
+    // Interfaces are 100% abstract except for "default methods"...
     // Interfaces do not accept constructors and private/protected/default methods / variables.
     // Multiple interfaces can be implemented to a subclass
 
@@ -21,6 +21,7 @@ interface myInterface {
     // variables:
     public String name = "Interface"; // no need for public to be written, it already has to be public
     final int seasons = 4; // no need for final to be written, it already has to be final
+    static double midnight = 00.00; // no need for static to be written, it's already static
 
 //    public String error1; // needs to be initialized since it has to be final
 //    private String error2 = "interface"; // cannot be private
