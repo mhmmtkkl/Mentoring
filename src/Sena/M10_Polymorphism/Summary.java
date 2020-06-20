1cd75740679542f17b80824d4e960651537d02ef
rsena <rsenaulgur@gmail.com>
2020-06-20 16:51:08 -0400

    new mentoring added

---
+++ src/Sena/M10_Polymorphism/Summary.java
@@ -0,0 +1,11 @@
+package Sena.M10_Polymorphism;
+
+public class Summary {
+
+    /** Presentation Link:
+     * https://docs.google.com/presentation/d/1BzteNWxq-dqePMJ2qdPazlDLG1LYSiHxZf0et0jo9WY/edit?usp=sharing
+     */
+
+
+
+}
---
+++ src/Sena/M10_Polymorphism/Tasks.java
@@ -0,0 +1,74 @@
+package Sena.M10_Polymorphism;
+
+public abstract class Tasks {
+}
+
+/**
+ * Task 1:
+ * // Create an abstract class named as IceCream :)
+ * // Have an int variable named as rolls,
+ * Have a constructor with the variable you have
+ **/
+
+
+
+// code here
+
+
+
+
+
+ /** Task 2:
+ * // Have a void method --> if roll number is 1, print "good for you :)"
+ * // if roll number is 2-3, print "guess been waiting for summer so bad, eh? :)"
+ * // if roll number is more than 3, print "woah, TOO MANY CALORIES"
+ * // else, print "Where is my ice creammmm???"
+ **/
+
+
+// code here
+
+
+
+
+
+/** Task 3:
+ // Have a void method --> if roll number is 1, print "good for you :)"
+ // if roll number is 2-3, print "guess been waiting for summer so bad, eh? :)"
+ // if roll number is more than 3, print "woah, TOO MANY CALORIES"
+ // else, print "Where is my ice creammmm???"
+ **/
+
+
+
+
+// code here
+
+
+
+
+
+/** Task 3:
+// Create at least 2 subclasses to IceCream, name them with your favorite ice cream flavors
+**/
+
+
+
+// code here
+
+
+
+
+
+/** Task 4:
+// Have an abstract void method as printProperties at IceCream class
+// Override the method from the subclasses and print the name of the flavor and number of rolls
+**/
+
+
+
+// code here
+
+
+
+
