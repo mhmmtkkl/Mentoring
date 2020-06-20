fe1c0794f01c2d443d6624b90c534979a2b06eca
rsena <rsenaulgur@gmail.com>
2020-06-20 17:53:45 -0400

    tasks solved

--- src/Sena/M10_Polymorphism/Tasks.java
+++ src/Sena/M10_Polymorphism/Tasks.java
@@ -1,6 +1,6 @@
 package Sena.M10_Polymorphism;
 
-public abstract class Tasks {
+public class Tasks {
 }
 
 /**
@@ -10,65 +10,90 @@ public abstract class Tasks {
  * Have a constructor with the variable you have
  **/
 
+abstract class IceCream {
+    int rolls;
+
+    public IceCream(int rolls) {
+        this.rolls = rolls;
+    }
+
+    public abstract void printProperties();
 
 
-// code here
+    /**
+     * Task 2:
+     * // Have a void method --> if roll number is 1, print "good for you :)"
+     * // if roll number is 2-3, print "guess been waiting for summer so bad, eh? :)"
+     * // if roll number is more than 3, print "woah, TOO MANY CALORIES"
+     * // else, print "Where is my ice creammmm???"
+     **/
+
+    public void countRolls() {
+        if (rolls > 3) {
+            System.out.println("woah, TOO MANY CALORIES");
+
+        } else if (rolls > 1) {
+            System.out.println("guess been waiting for summer so bad, eh? :)");
+
+        } else if (rolls > 0) {
+            System.out.println("good for you :)");
+
+        } else {
+            System.out.println("Where is my ice creammmm???");
+
+        }
+    }
+}
 
 
+    /**
+     * Task 3:
+     * // Create at least 2 subclasses to IceCream, name them with your favorite ice cream flavors
+     **/
 
+    class Lime extends IceCream {
 
+        public Lime(int rolls) {
+            super(rolls);
+        }
 
- /** Task 2:
- * // Have a void method --> if roll number is 1, print "good for you :)"
- * // if roll number is 2-3, print "guess been waiting for summer so bad, eh? :)"
- * // if roll number is more than 3, print "woah, TOO MANY CALORIES"
- * // else, print "Where is my ice creammmm???"
- **/
+        @Override
+        public void printProperties() {
+            System.out.println("Lime: " + rolls);
+        }
+    }
 
+    class chocolate extends IceCream {
 
-// code here
+        public chocolate(int rolls) {
+            super(rolls);
+        }
 
-
-
-
-
-/** Task 3:
- // Have a void method --> if roll number is 1, print "good for you :)"
- // if roll number is 2-3, print "guess been waiting for summer so bad, eh? :)"
- // if roll number is more than 3, print "woah, TOO MANY CALORIES"
- // else, print "Where is my ice creammmm???"
- **/
-
-
-
-
-// code here
-
-
-
-
-
-/** Task 3:
-// Create at least 2 subclasses to IceCream, name them with your favorite ice cream flavors
-**/
-
-
-
-// code here
-
-
-
+        @Override
+        public void printProperties() {
+            System.out.println("flavor name: " + getClass().getSimpleName());
+            System.out.println("roll number: " + super.rolls);
+        }
+    }
 
 
 /** Task 4:
-// Have an abstract void method as printProperties at IceCream class
-// Override the method from the subclasses and print the name of the flavor and number of rolls
-**/
+ // Have an abstract void method as printProperties at IceCream class
+ // Override the method from the subclasses and print the name of the flavor and number of rolls
+ **/
 
 
+ class myIceCreamOrder {
+    public static void main(String[] args) {
 
-// code here
+        IceCream o1 = new Lime(2);
+        o1.printProperties();
+        o1.countRolls();
 
+        IceCream o2 = new chocolate(1);
+        o2.printProperties();
+        o2.countRolls();
 
-
+    }
+}
 
