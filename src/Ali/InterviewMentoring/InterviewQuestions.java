f41e75d6b8a7151a423d3e9eb9b7b8cf513c6e55
mhmmtkkl <makoklu32@gmail.com>
2020-08-20 21:56:57 -0400

    interview

---
+++ src/Ali/InterviewMentoring/InterviewQuestions.java
@@ -0,0 +1,144 @@
+package Ali.InterviewMentoring;
+
+public class InterviewQuestions {
+
+
+        /*
+
+            1) Tell me about your project and also like roll?
+
+                Answer: How many years experience I have.
+                        Which tools I am using--> testNG, cucumber,java,selenium, sql,
+                                                    restAssured libraries.
+                                                    Personality.
+
+            2) How do you do database testing?
+
+                I am creating a citizenship and get the data from database make us it is created
+
+                    Usually testers doesn not have a create and delete function in the database.
+
+
+            3) Tell me your challenge with a dev team or business team?
+
+                Developer does not ID attribute so I asked them to created the ID attribute.
+                User story does not have a description or acceptance criteria.
+
+                I asked to team to have some happy hours so we will spend more time
+                    together and it help us to better communication.
+
+
+            4)Have you ever created framework from scratch and
+                Which technologies did you used and why?
+
+                    4.1)Can you tell me about your current framework?
+                    4.2)What kind of framework you can create?
+
+              I am using selenium with java.
+              Maven for project management tool.
+                    It helps me to organize my dependencies.
+
+              I use BDD-Page object model framework.
+               I am using BDD basically cucumber because every steps in cucumber feature files
+                  are reusable so I can write less code and I can test more functionalities.
+
+               POM(page object..) for creating each elements once in my project so
+                if we make a changes then it is easier and faster to update the project.
+
+                My team used to have testng report which has just scenario is failed or
+                       passed
+
+                But after I join I added extends report so it is showing us. How many
+                    scenarios passed and failed or skipped.
+
+               I create a runner classes with runner class I can run multiple feature files.
+
+
+               5)Project manager asked to you run all regression and you dont have a
+                enough time and how you can solve it?
+
+                I have a regression runner and I have a tag in the runner as well
+                    for example regression tag.
+
+                       I will add these tags to scenarious which I would like to run
+                       so in this way I can run my regrssion in one time(I can run it over night).
+
+                5)What do you do if regression automation scenario fails?
+                    First I will run them in automation as well.
+                    If it still fails.
+                        I will try to do this scenario in manual.
+
+                        If application has a defect I am creating a defect in jira.
+                        If it is passing manual I am fixing the automation code.
+
+
+                Question 6 Notes)In the basqar we have a user story which is adding an input to create fees.
+                        Name of the input Description.
+
+                        First developer will create a description input
+                        Then Tester will test the user story in manual.
+
+                6) When do you do automation? in the sprint or after the sprint?
+                       1) I am working in the sprint after the user story done then I am going to automate.
+                            After testing in manual.
+
+                Recommended 2)  After sprint ends I am changing automation script. Because in the sprint we might have
+                    some defects and also some user stories are huge so after all changes done and make sure functionalities
+                    are working as expected then I am working in automation.
+
+
+                7) We do not have a automation now. What is the steps you will take before starting automation?
+                        Which tools you will use how to decide it?
+
+                    First I would like to use Java selenium framework and also I am working with BDD page object model.
+                       I use BDD-Page object model framework.
+                       I am using BDD basically cucumber because every steps in cucumber feature files
+                          are reusable so I can write less code and I can test more functionalities.
+
+                       POM(page object..) for creating each elements once in my project so
+                        if we make a changes then it is easier and faster to update the project.
+
+                    These are tools I would like to use but if the company required different tools that is okay to me
+                        I can learn new tools and languages.
+
+
+                    I am going to create a word document for estimations and tools(I can add tool description).
+                        In the first month we are able to finish.
+                            10%
+                        In the second month we are able to finish.
+                            21%
+                        In the third month we are able to finish.
+                            35%
+
+
+                8) We have 1000 test cases which one you will start?
+
+                        If there is a captca selenium can not automate it.
+                        Voice record video selenium can not automate it.
+
+                        I would like to start with the pages which is reusable, used by users mostly.
+
+
+                9) What do you do after the sprint planing? Talk about your week?
+
+                      1)  Because I have a other QA which is mostly working with manual testing.
+                            He is start creating the test cases.
+
+                            And I am start working on the user stories which from the previous sprint.
+
+                      2)  In my current team I am the only QA.
+                            After the sprint planning I am creating the test cases.
+                                After test case creating is done then I am start working on the Automation.
+
+                           In my team developers are also helping me for testing in case if I need an help they can also help me.
+
+
+                10) Process for interview?
+
+
+
+
+                11) In student page how many test cases should we i mean numbers
+
+         */
+}
